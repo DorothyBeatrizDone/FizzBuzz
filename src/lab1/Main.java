55b699b03b6e6ee0e6c72a30f73bf2e328985051
dorothybeatrizdone <beatriz.done@cs.toronto.edu>
2026-09-11 14:17:17 -0400

    Main method is now compatible with it accepting a String. Added a method to convert String to Integer.

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -3,5 +3,6 @@ package lab1;
 public class Main {
     static void main(String[] args) {
         FizzBuzz.main(new String[]{"5"});
+        //Begin the entire FizzBuzz program at 5 and keep looping.
     }
 }
