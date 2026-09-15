a5f07341477f37356a2780e98bd2c75a91ccd0cf
dorothybeatrizdone <beatriz.done@cs.toronto.edu>
2026-09-15 15:33:40 -0400

    refactored to extract the helper function for second demo of lab 1

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -3,17 +3,53 @@ package lab1;
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
+//Task 1.1
+/*
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = Integer.parseInt(args[0]);
-        while (i < 100) {
+        int i = 1;
 
+        while (i < 100) {
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+            }
+            i++;
+        }
+    }
+}
+ */
+//Task 2
+class FizzBuzz {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i < 100) {
             i = doFizzBuzz(i);
         }
     }
 
-    public static int doFizzBuzz(int i) {
+    private static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
